49eedea7bb86c268382803aefeb6e462fc3ff339
Yaminadjenkal <mina20101951@hotmail.com>
2025-05-29 19:12:12 +0000

    second task

--- TypeScript/task_1/js/webpack.config.js
+++ TypeScript/task_1/js/webpack.config.js
@@ -1,22 +1,23 @@
-echo "const path = require('path');
+const path = require("path");
+
 module.exports = {
-  mode: 'development',
-  entry: './js/main.ts',
+  mode: "development", // ou 'production' selon tes besoins
+  entry: "./js/main.ts",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
-};" > ~/holbertonschool-web_react/TypeScript/task_1/webpack.config.js
+};
 
